f9f53c3978e6799f46eb297a320c738ad3aded42
Eric Feng <xikanfeng2@gmail.com>
2025-06-24 02:29:08 +0800

    finish largetable api

--- large_table_api/models.py
+++ large_table_api/models.py
@@ -66,3 +66,135 @@ class ArchaeaUnMAGTMHIndex(models.Model):
         indexes = [
             models.Index(fields=['archaea_id']),
         ]
+
+class FungiMAGProteinIndex(models.Model):
+    """用于索引所有Archaea文件的模型"""
+    archaea_id = models.CharField(max_length=255, unique=True)
+    file_path = models.TextField()
+    row_count = models.IntegerField(default=0)
+    
+    class Meta:
+        indexes = [
+            models.Index(fields=['archaea_id']),
+        ]
+
+class FungiMAGARGIndex(models.Model):
+    """用于索引所有Archaea文件的模型"""
+    archaea_id = models.CharField(max_length=255, unique=True)
+    file_path = models.TextField()
+    row_count = models.IntegerField(default=0)
+    
+    class Meta:
+        indexes = [
+            models.Index(fields=['archaea_id']),
+        ]
+
+class FungiMAGTMHIndex(models.Model):
+    """用于索引所有Archaea文件的模型"""
+    archaea_id = models.CharField(max_length=255, unique=True)
+    file_path = models.TextField()
+    row_count = models.IntegerField(default=0)
+    
+    class Meta:
+        indexes = [
+            models.Index(fields=['archaea_id']),
+        ]
+
+class FungiUnMAGProteinIndex(models.Model):
+    """用于索引所有Archaea文件的模型"""
+    archaea_id = models.CharField(max_length=255, unique=True)
+    file_path = models.TextField()
+    row_count = models.IntegerField(default=0)
+    
+    class Meta:
+        indexes = [
+            models.Index(fields=['archaea_id']),
+        ]
+
+class FungiUnMAGARGIndex(models.Model):
+    """用于索引所有Archaea文件的模型"""
+    archaea_id = models.CharField(max_length=255, unique=True)
+    file_path = models.TextField()
+    row_count = models.IntegerField(default=0)
+    
+    class Meta:
+        indexes = [
+            models.Index(fields=['archaea_id']),
+        ]
+
+class FungiUnMAGTMHIndex(models.Model):
+    """用于索引所有Archaea文件的模型"""
+    archaea_id = models.CharField(max_length=255, unique=True)
+    file_path = models.TextField()
+    row_count = models.IntegerField(default=0)
+    
+    class Meta:
+        indexes = [
+            models.Index(fields=['archaea_id']),
+        ]
+
+class VirusesMAGProteinIndex(models.Model):
+    """用于索引所有Archaea文件的模型"""
+    archaea_id = models.CharField(max_length=255, unique=True)
+    file_path = models.TextField()
+    row_count = models.IntegerField(default=0)
+    
+    class Meta:
+        indexes = [
+            models.Index(fields=['archaea_id']),
+        ]
+
+class VirusesMAGARGIndex(models.Model):
+    """用于索引所有Archaea文件的模型"""
+    archaea_id = models.CharField(max_length=255, unique=True)
+    file_path = models.TextField()
+    row_count = models.IntegerField(default=0)
+    
+    class Meta:
+        indexes = [
+            models.Index(fields=['archaea_id']),
+        ]
+
+class VirusesMAGTMHIndex(models.Model):
+    """用于索引所有Archaea文件的模型"""
+    archaea_id = models.CharField(max_length=255, unique=True)
+    file_path = models.TextField()
+    row_count = models.IntegerField(default=0)
+    
+    class Meta:
+        indexes = [
+            models.Index(fields=['archaea_id']),
+        ]
+
+class VirusesUnMAGProteinIndex(models.Model):
+    """用于索引所有Archaea文件的模型"""
+    archaea_id = models.CharField(max_length=255, unique=True)
+    file_path = models.TextField()
+    row_count = models.IntegerField(default=0)
+    
+    class Meta:
+        indexes = [
+            models.Index(fields=['archaea_id']),
+        ]
+
+class VirusesUnMAGARGIndex(models.Model):
+    """用于索引所有Archaea文件的模型"""
+    archaea_id = models.CharField(max_length=255, unique=True)
+    file_path = models.TextField()
+    row_count = models.IntegerField(default=0)
+    
+    class Meta:
+        indexes = [
+            models.Index(fields=['archaea_id']),
+        ]
+
+class VirusesUnMAGTMHIndex(models.Model):
+    """用于索引所有Archaea文件的模型"""
+    archaea_id = models.CharField(max_length=255, unique=True)
+    file_path = models.TextField()
+    row_count = models.IntegerField(default=0)
+    
+    class Meta:
+        indexes = [
+            models.Index(fields=['archaea_id']),
+        ]
