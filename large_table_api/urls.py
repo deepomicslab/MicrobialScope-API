0810b0f4154966dc10edf307d816804c72214a5f
Eric Feng <xikanfeng2@gmail.com>
2025-06-24 01:54:32 +0800

    finish archaea largetable api

--- large_table_api/urls.py
+++ large_table_api/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('archaea_protein_list/', views.archaea_protein_list, name='archaea_protein_list'),
+    path('archaea_arg_list/', views.archaea_arg_list, name='archaea_arg_list'),
+    path('archaea_tmh_list/', views.archaea_tmh_list, name='archaea_tmh_list'),
+    path('archaea_unmag_protein_list/', views.archaea_unmag_protein_list, name='archaea_unmag_protein_list'),
+    path('archaea_unmag_arg_list/', views.archaea_unmag_arg_list, name='archaea_unmag_arg_list'),
+    path('archaea_unmag_tmh_list/', views.archaea_unmag_tmh_list, name='archaea_unmag_tmh_list'),
 ]
