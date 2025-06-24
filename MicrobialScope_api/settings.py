cc9ffa7eac5852b7bdd6f1ed9ba981f5cff04dcd
Eric Feng <xikanfeng2@gmail.com>
2025-06-24 22:31:36 +0800

    add gzip compress middleware

--- MicrobialScope_api/settings.py
+++ MicrobialScope_api/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.gzip.GZipMiddleware",
 ]
 
 CORS_ALLOWED_ORIGINS = [
